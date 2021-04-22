cc86e5f03f80cdac9d7d5093003ee245ea07b57d
Francisco Melicias <franciscomelicias@MBPdeFrancisco.lan>
2021-04-22 21:53:38 +0100

    scramble mutacao feita

--- src/ga/geneticOperators/Mutation2.java
+++ src/ga/geneticOperators/Mutation2.java
@@ -13,14 +13,25 @@ public class Mutation2<I extends IntVectorIndividual, P extends Problem<I>> exte
     @Override
     public void mutate(I ind) {
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
-
+        int i1 = GeneticAlgorithm.random.nextInt(ind.getNumGenes()-1)+1; //indice 1 para trocar
+        int i2 = GeneticAlgorithm.random.nextInt(ind.getNumGenes()-1)+1; //indice 2 para trocar
+        while(i1 >= i2){ //caso o indice 2 seja maior que o indice 1 random aoutra vez
+            i1 = GeneticAlgorithm.random.nextInt(ind.getNumGenes()-1)+1;
+            i2 = GeneticAlgorithm.random.nextInt(ind.getNumGenes()-1)+1;
+        }
+        for(int i = 0; i < i2-i1; i++){ // sacar de 2 numeros para dar swap entre o intervalo de i1 e i2
+            int s1 = GeneticAlgorithm.random.nextInt(i1)+(i2-i1);
+            int s2 = GeneticAlgorithm.random.nextInt(i1)+(i2-i1);
+            int first = ind.getGene(s1);
+            ind.setGene(s1,ind.getGene(s2));
+            ind.setGene(s2,first);
+        }
     }
 
 
     @Override
     public String toString() {
         //TODO
-        return "";
+        return "Scramble mutation (" + probability + ")";
     }
 }
