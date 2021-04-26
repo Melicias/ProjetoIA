77e136226b0b7fbf0bc1dbac43177e0901483163
DanielSarreira <33024080+DanielSarreira@users.noreply.github.com>
2021-04-26 19:07:04 +0100

    Recombination3loading

--- src/ga/geneticOperators/Recombination3.java
+++ src/ga/geneticOperators/Recombination3.java
@@ -15,10 +15,44 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
         super(probability);
     }
 
+    private int[] child1, child2;
+
     @Override
     public void recombine(I ind1, I ind2) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        child1 = new int[ind1.getNumGenes()];
+        child2 = new int[ind2.getNumGenes()];
+
+        int i1 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes()-1)+1;
+        int i2 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes()-1)+1;
+        while(i1 >= i2){
+            i1 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes()-1)+1;
+            i2 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes()-1)+1;
+        }
+
+        for (int i = i1; i < i2; i++) {
+            child1[i] = child2[i];
+            child2[i] = child1[i];
+        }
+        int numGenes = ind1.getNumGenes();
+
+        int indAux = i2;
+        while (indAux != i1){
+            if (check_forDuplicates(child1, ind2)) {
+
+            }
+        }
+
+    }
+
+    private boolean check_forDuplicates(int[] offspring, int indexOfElement) {
+        for (int index = 0; index < offspring.length; index++) {
+            if ((offspring[index] == offspring[indexOfElement]) &&
+                    (indexOfElement != index)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
