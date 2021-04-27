fef1cd75f216c3c9bced2cc56d416bfe5b758e09
Francisco Melicias <franciscomelicias@MBPdeFrancisco.lan>
2021-04-27 16:05:56 +0100

    novo coiso adicionado, feito com o sarreira, talvez seja ncessario melhor, verificar o seu comportamente nos proximos teste (recombinacao3)

--- src/ga/geneticOperators/Recombination3.java
+++ src/ga/geneticOperators/Recombination3.java
@@ -17,6 +17,7 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
 
     private int[] child1, child2;
 
+
     @Override
     public void recombine(I ind1, I ind2) {
 
@@ -30,25 +31,43 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
             i2 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes()-1)+1;
         }
 
-        for (int i = i1; i < i2; i++) {
-            child1[i] = child2[i];
-            child2[i] = child1[i];
+        for (int i = i1; i <= i2; i++) {
+            child1[i] = ind1.getGene(i);
+            child2[i] = ind2.getGene(i);
         }
-        int numGenes = ind1.getNumGenes();
-
-        int indAux = i2;
-        while (indAux != i1){
-            if (check_forDuplicates(child1, ind2)) {
-
-            }
+        System.out.println("Entrou");
+        child1 = fillChilds(child1,ind2,i1,i2+1);
+        child2 = fillChilds(child2,ind1,i1,i2+1);
+        System.out.println("saiu");
+        for (int i = 0; i < ind1.getNumGenes(); i++) {
+            ind1.setGene(i, child1[i]);
+            ind2.setGene(i, child2[i]);
         }
 
     }
 
-    private boolean check_forDuplicates(int[] offspring, int indexOfElement) {
+
+    private int[] fillChilds(int[] child,I ind,int i1, int i2){
+        int indAux = i2;
+        while (i2 != i1){
+            if(i2 == ind.getNumGenes()) {
+                i2 = 0;
+            }
+            do{
+                indAux++;
+                if(indAux >= ind.getNumGenes()) {
+                    indAux = 0;
+                }
+            }while(check_forDuplicates(child, ind.getGene(indAux)));
+            child[i2] = ind.getGene(indAux);
+            i2++;
+        }
+        return child;
+    }
+
+    private boolean check_forDuplicates(int[] offspring, int element) {
         for (int index = 0; index < offspring.length; index++) {
-            if ((offspring[index] == offspring[indexOfElement]) &&
-                    (indexOfElement != index)) {
+            if (offspring[index] == element) {
                 return true;
             }
         }
@@ -58,6 +77,6 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
     @Override
     public String toString(){
         //TODO
-        return "";
+        return "crossover";
     }    
 }
